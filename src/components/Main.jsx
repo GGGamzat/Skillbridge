8b631641eaa571fdff4004a84d161d76d2ac4d84
GGGamzat <abdullayevgamzat@gmail.com>
2025-01-16 00:30:39 +0300

    create main-screen

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,18 @@
+import play from '../assets/icons/play.svg';
 
 function Main() {
     return(
         <main className="main">
-            <div class="main_image"></div>
+            <div class="main__screen">
+                <div className="main-screen__body">
+                    <h1 className="main-body__title">The Best Streaming Experience</h1>
+                    <p className="main-body__text">StreamVibe is the best streaming experience for watching your favorite movies and shows on demand, anytime, anywhere. With StreamVibe, you can enjoy a wide variety of content, including the latest blockbusters, classic movies, popular TV shows, and more. You can also create your own watchlists, so you can easily find the content you want to watch.</p>
+                    <button type="button" className="main-body__btn">
+                        <img src={play} alt="" />
+                        <span>Start Watching Now</span>
+                    </button>
+                </div>
+            </div>
         </main>
     );
 }
